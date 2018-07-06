43cf6ba9dac43bb0abf648c29cbc5fe3190315fa
Дмитрий Сергеевич <tudok124@gmail.com>
2018-07-06 17:02:02 +0300

    Торва
    ПРобуем писать кнопку корзины

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -17,7 +17,12 @@ export default class User extends PureComponent {
         super(props);
 
         this.state = {
-            counter: 1
+            counter: 1,
+            cadr:{
+                titel:'',
+                id:''
+            },
+            outline:true
         }
     }
     static propTypes = {
@@ -47,33 +52,42 @@ export default class User extends PureComponent {
             counter: counter + 1
         });
     }
+    handleAddClick = () => {
+        const { counter } = this.state;
+
+        this.setState({
+            counter: counter + 1
+        });
+    }
 
     render() {
         const {user} = this.props;
         const { counter } = this.state;
         //const description = <Description name={user.name} />;
         const element = <Welcome name="Sara" />;
-        //const Favorites = <Favorites name="Sara" />;
-        // const Tocart = <Tocart name="Sara" />;
+       const status =()=>{
+           if (!this.state.outline){
+              let status="outline";
+           }
+       }
         return (
                 <div sm="6" to={`/users/${user.id}`} class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
                     <Card>
                         <CardBody>
                             <CardTitle>{user.title}</CardTitle>
                             <CardText>
-                               ыавываы
+                                {user.body}
                             </CardText>
                             <CardText>
                                 <FormGroup row>
-                                    <Button  outline  size="sm" color="link" onClick={this.handleMinusClick}><Remove /></Button>
+                                    <Button    size="sm" color="link" onClick={this.handleMinusClick}><Remove /></Button>
                                     {' '}
-                                    <Input bsSize="sm"    color="primary" type="text" name="text" value={ counter } />                               {' '}
-                                    <Button  outline size="sm"   color="link" onClick={this.handlePlusClick}><Add /></Button>
+                                        <Button    size="sm" color="link" onClick={this.handleMinusClick}>{ counter } шт.</Button>
+                                    {' '}
+                                        <Button   size="sm"   color="link" onClick={this.handlePlusClick}><Add /></Button>
 
-                                    <Button outline size="sm" color="primary"><AddCard /></Button>{' '}
+                                    <Button onClick={this.handleAddClick} {status}  size="sm" color="primary"><AddCard /></Button>{' '}
                                 </FormGroup>
-
-
                             </CardText>
                         </CardBody>
                     </Card>
@@ -101,3 +115,5 @@ function App(price,name) {
 }
 //https://redux-form.com/6.5.0/examples/simple///
 //icon react http://react-icons.github.io/react-icons/md.html
+
+//redux https://github.com/appticles/pwa-theme-woocommerce
